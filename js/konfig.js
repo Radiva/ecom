90082fe8c01b26e7378bdb6bb6e8889cbaced1e1
radiva <28networking@gmail.com>
2017-08-10 12:07:53 +0700

    commit

--- js/konfig.js
+++ js/konfig.js
@@ -1,7 +1,7 @@
 function base_url(url){
     var url = url;
-    //var base = 'http://127.0.0.1/ecommerce/';
-    var base = 'http://192.168.100.6/ecommerce/';
+    var base = 'http://127.0.0.1/ecommerce/';
+    //var base = 'http://192.168.100.6/ecommerce/';
     return base+url;
     
 }
